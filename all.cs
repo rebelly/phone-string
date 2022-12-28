630d4e8eedf80d2e5e2f3f08c22c1eb5e9c00495
Sergey <114340735+rebelly@users.noreply.github.com>
2022-12-28 18:47:44 +0300

    Update all.cs

--- all.cs
+++ all.cs
@@ -13,15 +13,15 @@ class Program
     }
     static void same_lenth(string n1, ref string n2)
     {
-        int o = 0;
-        string check = "";
-        while (n1.Length != n2.Length)
+
+        while (n1.Length > n2.Length)
         {
-            check += "0";
-            n2 = check + n2;
+            
+            n2 = "0" + n2;
+            
         }
     }
-    static void l_m_pls(string n1, string n2)
+    static string l_m_pls(string n1, string n2)
     {
 
         if (n1.Length <= n2.Length)
@@ -67,13 +67,69 @@ class Program
             }
             else res = Convert.ToString(Convert.ToInt32(res[0]) - 48);
         }
-        Console.WriteLine(res);
+        return res;
         
+    }
+    static string l_m_minus(string n1, string n2)
+    {
+        bool min = true;
+        if (n1.Length > n2.Length && n1[0] > n2[0]) min = false;
+        else
+        {
+            string temp = n1;
+            n1 = n2;
+            n2 = temp;
+
+        }
+        same_lenth(n1, ref n2);
+        int cur;
+        string res = "";
+        bool nxt = false;
+        for (int i = n1.Length-1; i > -1; i--)
+        {
+            {
+                cur = Convert.ToInt32((Convert.ToInt32(n1[i]) - 48) - (Convert.ToInt32(n2[i]) - 48));
+                if (nxt)
+                {
+                    cur--;
+                    nxt = false;
+                }
+                if (cur >= 0)
+                {
+                    res += cur;
+                }
+                else
+                {
+                    
+                    cur += 10;
+                    res += cur;
+                    nxt = true;
+                }
+            }
+        }
+        res = reverse_string(res);
+        if (min) res = "-" + res;
+        return res;
     }
     public static void Main()
     {
+        Console.WriteLine("Введите первое число");
         string n1 = Console.ReadLine();
+        Console.WriteLine("Введите второе число");
         string n2 = Console.ReadLine();
-        l_m_pls(n1, n2);
+        Console.WriteLine("Введите знак записанный между числами");
+        string sign = Console.ReadLine();
+        if (sign == "+")
+        {
+            Console.WriteLine($"{n1} + {n2} = {l_m_pls(n1, n2)}");
+        }
+        else if(sign == "-")
+        {
+            Console.WriteLine($"{n1} - {n2} = {l_m_minus(n1, n2)}");
+        }
+        else
+        {
+            Console.WriteLine("Неизвестный знак, Введите либо '+' либо '-' ");
+        }
     }
 }
